7783e9a9c516e93dc90404fd4f9e3fe6ad74b23f
Kirill <kirillahmadullin@gmail.com>
2022-11-24 16:50:32 +0500

    Программа, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)

--- Solving_tasks_1/Task_3.py
+++ Solving_tasks_1/Task_3.py
@@ -1,12 +1,12 @@
 # Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-number_x = int (input (f'Введите значение X (не равен 0): '))
+number_x = int (input ('Введите значение X (не равен 0): '))
 while number_x == 0:
-    number_x = int (input (f'Введите значение X (не равен 0): '))
+    number_x = int (input ('Введите значение X (не равен 0): '))
 
-number_y = int (input (f'Введите значение Y (не равен 0): '))
+number_y = int (input ('Введите значение Y (не равен 0): '))
 while number_y == 0:
-    number_y = int (input (f'Введите значение Y (не равен 0): '))
+    number_y = int (input ('Введите значение Y (не равен 0): '))
 
 if number_x > 0 and number_y > 0: print ("1")
 if number_x < 0 and number_y > 0: print ("2")
