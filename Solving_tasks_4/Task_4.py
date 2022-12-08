287ed89f8f417b34950ac710690f919235522df0
Kirill <kirillahmadullin@gmail.com>
2022-12-09 03:05:59 +0500

    Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k

--- Solving_tasks_4/Task_4.py
+++ Solving_tasks_4/Task_4.py
@@ -10,29 +10,40 @@ def f(x):
     while x >= 0:
         a = random.randint(0, 100)
         
-        if x > 1 and a > 1:
-            with open (file_path, 'a') as f_data:
-                f_data.write (f'{a}*x^{x} + ')
-                
-        if x > 1 and a == 1:
-            with open (file_path, 'a') as f_data:
-                f_data.write (f'x^{x} + ')
-
-        if x > 1 and a == 0:
-            with open (file_path, 'a') as f_data:
-                f_data.write (' + ')
+        if x > 1:
+            if a > 1:
+                with open (file_path, 'a') as f_data:
+                    f_data.write (f'{a}*x^{x}')
+            if a == 1:
+                with open (file_path, 'a') as f_data:
+                    f_data.write (f'x^{x}')
+            if a == 0:
+                with open (file_path, 'a') as f_data:
+                    f_data.write ('')    
         
-        if x == 1 and a > 1:
-            with open (file_path, 'a') as f_data:
-                f_data.write (f'{a}*x + ')
+        if x == 1: 
+            if a > 1:
+                with open (file_path, 'a') as f_data:
+                    f_data.write (f'{a}*x')
+            if a == 0:
+                with open (file_path, 'a') as f_data:
+                    f_data.write ('')
         
-        if x == 0 and a > 0:
-            with open (file_path, 'a') as f_data:
-                f_data.write (f'{a} = 0')
+        if x == 0:
+            if a > 0:
+                with open (file_path, 'a') as f_data:
+                    f_data.write (f'{a}')
     
-        if x == 0 and a == 0:
+            if a == 0:
+                with open (file_path, 'a') as f_data:
+                    f_data.write ('')
+        
+        if x != 0:
             with open (file_path, 'a') as f_data:
-                f_data.write (' = 0')
+                    f_data.write (' + ')
+        else:
+            with open (file_path, 'a') as f_data:
+                    f_data.write (' = 0')
         
         return f(x-1)
 
