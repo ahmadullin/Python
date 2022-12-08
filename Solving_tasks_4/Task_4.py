fc0860f08e6ad2c568145912b46f8cd79e73bb01
Kirill <kirillahmadullin@gmail.com>
2022-12-09 00:34:11 +0500

    Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k

---
+++ Solving_tasks_4/Task_4.py
@@ -0,0 +1,44 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+import random, os
+
+while (num := int (input ("Введите натуральное число: "))) < (0):
+    print ("Попробуйте снова...")
+
+#f(random.randint(0, 100), num)
+file_path = r"C:\Users\1\Desktop\Python\Solving_tasks_4\Task_4.txt"
+def f(x):
+    while x >= 0:
+        a = random.randint(0, 100)
+        
+        if x != 0 and a > 1:
+            with open (file_path, 'a') as f_data:
+                f_data.write (f'{a}*x^{x}')
+                f_data.write (' + ')
+                
+        if x != 0 and a == 1:
+            with open (file_path, 'a') as f_data:
+                f_data.write (f'x^{x}')
+                f_data.write (' + ')
+
+        if x != 0 and a == 0:
+            with open (file_path, 'a') as f_data:
+                f_data.write (' + ')
+        
+        if x == 0 and a !=0:
+            with open (file_path, 'a') as f_data:
+                f_data.write (f'{a} = 0')
+    
+        if x == 0 and a == 0:
+            with open (file_path, 'a') as f_data:
+                f_data.write (' = 0')
+        
+
+        return f(x-1)
+
+f(num)
+
+with open (file_path, 'r') as f_data:
+    print (f_data.readline ())
+
+os.remove(file_path)
