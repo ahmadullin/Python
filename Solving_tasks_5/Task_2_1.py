6602901ae8a57ec5cf34a3f791b51a6d1a877850
Kirill <kirillahmadullin@gmail.com>
2022-12-12 20:19:48 +0500

    Игра в 'конфетки' Игрок/Глупый Бот

---
+++ Solving_tasks_5/Task_2_1.py
@@ -0,0 +1,55 @@
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой.
+# За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. 
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+import random
+print ("Жеребьевка: определяется первый ход с помощью игрального кубика")
+
+kub_player = random.randint(1, 6)
+print (f"Значение игрального кубика Игрок: {kub_player}")
+kub_bot = random.randint(1, 6)
+print (f"Значение игрального кубика Бот: {kub_bot}")
+
+if kub_player > kub_bot:
+    player_1 = 'Игрок'
+    player_2 = 'Бот'
+    i = 0
+    print (f"Начинает {player_1}")
+else:
+    player_1 = 'Игрок'
+    player_2 = 'Бот'
+    i = 1
+    print (f"Начинает {player_2}")
+
+def play(num, i):
+
+    print (f"\nОсталось {num} конфет")
+    
+    if i == 0:
+        player = player_1
+        print (f"Ход {player}")
+
+        x = int (input ("Можно взять от 1 до 28 конфет. Сколько конфет возьмешь? --> "))
+        while (x > 29) or (x < 1):
+            x = int (input ("Введи значение от 1 до 28 --> "))
+        while x > num:
+            x = int (input (f"Осталось не так много конфет. Введи значение от 1 до {num} --> "))
+
+    if i == 1:
+        player = player_2
+        print (f"Ход {player}")
+        x = random.randint(1, 28)
+        print (f"Бот взял {x} конфет")
+        while x > num:
+            x = random.randint(1, num)
+
+    
+    result = num - x
+    if result > 0 and i == 0: return play(num - x, i+1)
+    if result > 0 and i == 1: return play(num - x, i-1)
+    else: print (f"Победил {player}")
+
+        
+num = int (input ("Сколько будет конфет в игре: "))
+
+play (num, i)
