5d17f7df915400a712f7f7fb9f6867fa7f7babd4
Kirill <kirillahmadullin@gmail.com>
2022-12-21 11:06:50 +0500

    Search contact

--- Solving_tasks_7/body_search.py
+++ Solving_tasks_7/body_search.py
@@ -3,6 +3,7 @@ import view
 
 file_path = table.path()
 log_table = table.log()
+x = table.log()
 
 def call_log_search(contact):
     count = 0
@@ -18,6 +19,9 @@ def call_log_search(contact):
         if count == 1: print (log_table)
         if count > 1:
             print (log_table)
-            num = view.interview_search_2()
-            table = log_table.get_string(start = num - 1, end = num)
-            print (table)
+            num = f'{view.interview_search_2()}'
+            with open (file_path, 'r') as f_data:
+                for i in f_data.readlines():
+                    if num == i[0]:
+                        x.add_row(i.split(' '))
+                    print(f'\n {x}')
