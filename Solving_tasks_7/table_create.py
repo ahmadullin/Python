e6c1ef399b3fd113b0a170c9fbeaedbe7b9e8abe
Kirill <kirillahmadullin@gmail.com>
2022-12-21 11:07:55 +0500

    Create table

---
+++ Solving_tasks_7/table_create.py
@@ -0,0 +1,10 @@
+from prettytable import PrettyTable
+
+def log():
+    log_table = PrettyTable()
+    log_table.field_names = ['id', 'Имя', 'Фамилия','Номер телефона']
+    return log_table
+    
+def path():
+    file_path = r'C:\Users\1\Desktop\Python\call_log.txt'
+    return file_path
